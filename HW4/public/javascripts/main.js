e915351aed84d51754c81092b740c35e8c8321c5
sohamsavalapurkar <64315189+sohamsavalapurkar@users.noreply.github.com>
2021-11-17 18:36:56 -0600

    Add files via upload

---
+++ HW4/public/javascripts/main.js
@@ -0,0 +1,43 @@
+$(function(){
+
+    $.ajax({
+        method:'GET',
+        url: 'api/videos',
+        success: function(videos) {
+            $.each(videos, function(i, video) {
+                $("#videolist").append('<li>' + video.title + "</li>");
+
+                //$("#videolist").append('<li style="list-style-type:none; float:left; padding: 16px"><img src="../images/'+ video.image +'" style="width: 200px; height: 200px"><br>' + video.title + '</li>');
+            });
+        },
+        error: function() {
+            alert("Error lodaing videos");
+        }
+    });
+    $('#add').click(function() {
+        var vtitle = $("#title").val();
+        var vgenre = $("#genre").val();
+        var vdesc = $("#desc").val();
+
+        var video = {
+            title: vtitle,
+            genre: vgenre,
+            desc: vdesc
+        }
+
+        $.ajax({
+            method:'POST',
+            url: 'api/videos',
+            data: video,
+            success: function(newVideo) {
+               
+                $("#videolist").append('<li>' + newVideo.title + "</li>");
+    
+               
+            },
+            error: function() {
+                alert("Error lodaing videos");
+            }
+        });
+    })
+})
